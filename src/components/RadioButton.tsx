e73ff2a3fe69c9900086b1b6fb8a90a3d1f7372e
Liam <liam.going@code.berlin>
2024-09-02 13:58:50 +0200

    [update] radio button

---
+++ src/components/RadioButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface RadioButtonProps {
+    name: string;
+    value: string;
+    checked: boolean;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    label: string;
+}
+
+const RadioButton: React.FC<RadioButtonProps> = ({ name, value, checked, onChange, label }) => {
+    return (
+        <div className="radio-group">
+            <input
+                type="radio"
+                name={name}
+                value={value}
+                checked={checked}
+                onChange={onChange}
+            />
+            <label>{label}</label>
+        </div>
+    );
+};
+
+export default RadioButton;
