c4e6055d6046415b61fa72f053c5f243076bc21d
Liam <liam.going@code.berlin>
2024-09-02 14:41:23 +0200

    [update] input component for PersonalInfo page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,15 +5,17 @@ interface InputProps {
     value: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     placeholder?: string;
+    name: string,
 }
 
-const Input: React.FC<InputProps> = ({ type, value, onChange, placeholder }) => {
+const Input: React.FC<InputProps> = ({ type, value, onChange, placeholder, name }) => {
     return (
         <input
             type={type}
             value={value}
             onChange={onChange}
             placeholder={placeholder}
+            name={name}
         />
     );
 };
