cef0c689377ff2be37948f7e13c25ecb2e19c413
Liam <liam.going@code.berlin>
2024-09-02 14:41:57 +0200

    [update] app routes and index entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import AppRoutes from './routes';
-import ProgressIndicator from './components/ProgressIndicator';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import store from './context/store';
+import PersonalInfo from './pages/PersonalInfo';
+import SalaryIndications from './pages/SalaryIndications';
+import Summary from './pages/Summary';
+import NotFound from './pages/NotFound';
+import Header from './components/Header';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <ProgressIndicator />
-      <AppRoutes />
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <div className="app">
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<h1>Welcome to Buena</h1>} />
+                        <Route path="/personal-info" element={<PersonalInfo />} />
+                        <Route path="/salary-indications" element={<SalaryIndications />} />
+                        <Route path="/summary" element={<Summary />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </div>
+            </Router>
+        </Provider>
+    );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './assets/styles/index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { FormProvider } from './context/FormProvider';
-import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root') as HTMLElement;
-
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <FormProvider>
+ReactDOM.render(
+    <React.StrictMode>
         <App />
-      </FormProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-reportWebVitals();
