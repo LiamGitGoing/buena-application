354a851a5e7209da5e749c14575988a8f998a66d
Liam <liam.going@code.berlin>
2024-09-04 12:33:57 +0200

    [chores] ReactDOM.render was deprecated -- updated to use root.render

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './context/store';
 import App from './App';
 
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container!);
+
+root.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
             <App />
         </PersistGate>
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 );
