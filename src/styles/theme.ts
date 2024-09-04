684bbaad6959f92debca7cf01b9bd83a760590ec
Liam <liam.going@code.berlin>
2024-09-04 14:29:22 +0200

    [update] font added

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,16 +1,18 @@
 export const theme = {
-    colors: {
-      black: '#0c0a09',
-      white: '#ffffff',
-      darkGrey: '#78716c',
-      lightGrey: '#f5f5f4',
-      darkYellow: '#f2b13c',
-      lightYellow: '#f4c87f',
-    },
-    spacing: {
-      mobile: '8px',
-      tablet: '16px',
-      desktop: '-1px',
-    },
-  };
-  
+  colors: {
+    black: "#0c0a09",
+    white: "#ffffff",
+    darkGrey: "#78716c",
+    lightGrey: "#f5f5f4",
+    darkYellow: "#f2b13c",
+    lightYellow: "#f4c87f",
+  },
+  spacing: {
+    mobile: "8px",
+    tablet: "16px",
+    desktop: "24px",
+  },
+  fonts: {
+    primary: "Inter Display",
+  },
+};
