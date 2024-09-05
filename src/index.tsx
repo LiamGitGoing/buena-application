f395ef7164905f9e945f1a2dda1aded1b56f15cb
Liam <liam.going@code.berlin>
2024-09-05 13:41:16 +0200

    [chores] formatting

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,13 @@ import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './context/store';
 import App from './App';
 
-
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container!);
 
 root.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <App />
-        </PersistGate>
-    </Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
