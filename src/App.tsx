424f548d791cf5b399f287b2d1a42608ca545f3b
Liam <liam.going@code.berlin>
2024-09-05 19:20:55 +0200

    [update] Ant primary theme override

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,40 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import store from "./context/store";
-import PersonalInfo from "./pages/PersonalInfo";
-import SalaryIndications from "./pages/SalaryIndications";
-import Summary from "./pages/Summary";
-import NotFound from "./pages/NotFound";
-import Header from "./components/Header";
-import EntryPage from "./pages/EntryPage";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./styles/theme";
-import "./styles/global.css";
-import { Wrapper } from "./AppStyled";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import store from './context/store';
+import PersonalInfo from './pages/PersonalInfo';
+import SalaryIndications from './pages/SalaryIndications';
+import Summary from './pages/Summary';
+import NotFound from './pages/NotFound';
+import Header from './components/Header';
+import EntryPage from './pages/EntryPage';
+import { ThemeProvider } from 'styled-components';
+import { ConfigProvider } from 'antd';
+import { antTheme } from './styles/antTheme';
+import { theme } from './styles/theme';
+import './styles/global.css';
+import { Wrapper } from './AppStyled';
 
 const App: React.FC = () => {
   return (
     <Wrapper>
       <Provider store={store}>
         <ThemeProvider theme={theme}>
-          <Router>
-            <div className="app">
+          <ConfigProvider theme={antTheme}>
+            <Router>
               <Header />
               <Routes>
-                <Route path="/" element={<EntryPage />} />
-                <Route path="/personal-info" element={<PersonalInfo />} />
+                <Route path='/' element={<EntryPage />} />
+                <Route path='/personal-info' element={<PersonalInfo />} />
                 <Route
-                  path="/salary-indications"
+                  path='/salary-indications'
                   element={<SalaryIndications />}
                 />
-                <Route path="/summary" element={<Summary />} />
-                <Route path="*" element={<NotFound />} />
+                <Route path='/summary' element={<Summary />} />
+                <Route path='*' element={<NotFound />} />
               </Routes>
-            </div>
-          </Router>
+            </Router>
+          </ConfigProvider>
         </ThemeProvider>
       </Provider>
     </Wrapper>
---
+++ src/styles/antTheme.ts
@@ -0,0 +1,8 @@
+import { theme } from './theme';
+
+export const antTheme = {
+  token: {
+    colorPrimary: theme.colors.black,
+    colorPrimaryHover: theme.colors.darkGrey,
+  },
+};
