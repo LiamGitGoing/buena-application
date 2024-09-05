db61efa81a54ec2bc8f4d3e173bf8fdcc6d7dc50
Liam <liam.going@code.berlin>
2024-09-05 19:19:47 +0200

    [update] Entrypage with mp4 and styling

--- src/pages/EntryPage.tsx
+++ src/pages/EntryPage.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
+import Card from '../components/Card';
+import { TitleWrapper } from './shared/TextStyled';
+import ProgressIndicator from '../components/ProgressIndicator';
+import { ButtonWrapper, VideoWrapper } from './EntryPageStyled';
+import hero from '../assets/videos/hero.mp4';
 import Button from '../components/Button';
 
 const EntryPage: React.FC = () => {
-    const navigate = useNavigate(); 
-    const handleNext = () => {
-        navigate('/personal-info'); 
-    };
-    return (
-        <div>
-            <h2>Welcome to your Buena application form!</h2>
-            <Button onClick={handleNext}>Start</Button>
-        </div>
-    );
+  const navigate = useNavigate();
+  const handleNext = () => {
+    navigate('/personal-info');
+  };
+  return (
+    <Card>
+      <ProgressIndicator isRoot={true} />
+      <TitleWrapper>Welcome to your Buena application form!</TitleWrapper>
+      <VideoWrapper>
+        <video
+          src={hero}
+          autoPlay
+          muted
+          loop
+          style={{ width: '90%', borderRadius: '8px' }}
+        />
+      </VideoWrapper>
+      <ButtonWrapper>
+        <Button text='Next' onClick={handleNext} />
+      </ButtonWrapper>
+    </Card>
+  );
 };
 
 export default EntryPage;
---
+++ src/pages/EntryPageStyled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+`;
+
+export const VideoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
