f2a4a8000d7e30a6ef66279c3f805c752b557d22
Liam <liam.going@code.berlin>
2024-09-05 19:21:52 +0200

    [update] customized button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React from 'react';
+import { Button as AntButton } from 'antd';
 
 interface ButtonProps {
+  text: string;
   onClick: () => void;
-  children: React.ReactNode;
+  link?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
-  return <button onClick={onClick}>{children}</button>;
+const Button: React.FC<ButtonProps> = ({ text, onClick, link }) => {
+  return link ? (
+    <a href={link} style={{ textDecoration: 'none' }}>
+      <AntButton type='primary' onClick={onClick}>
+        {text}
+      </AntButton>
+    </a>
+  ) : (
+    <AntButton type='primary' onClick={onClick}>
+      {text}
+    </AntButton>
+  );
 };
 
 export default Button;
