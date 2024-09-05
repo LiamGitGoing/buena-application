bfa87d697165d863375f5ac799877ef08531bbbe
Liam <liam.going@code.berlin>
2024-09-05 11:46:54 +0200

    [update] App overall padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import store from './context/store';
-import PersonalInfo from './pages/PersonalInfo';
-import SalaryIndications from './pages/SalaryIndications';
-import Summary from './pages/Summary';
-import NotFound from './pages/NotFound';
-import Header from './components/Header';
-import EntryPage from './pages/EntryPage';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import store from "./context/store";
+import PersonalInfo from "./pages/PersonalInfo";
+import SalaryIndications from "./pages/SalaryIndications";
+import Summary from "./pages/Summary";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import EntryPage from "./pages/EntryPage";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+import "./styles/global.css";
+import { Wrapper } from "./AppStyled";
 
 const App: React.FC = () => {
-    return (
-        <Provider store={store}>
-            <ThemeProvider theme={theme}>
-            <Router>
-                <div className="app">
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<EntryPage />} />
-                        <Route path="/personal-info" element={<PersonalInfo />} />
-                        <Route path="/salary-indications" element={<SalaryIndications />} />
-                        <Route path="/summary" element={<Summary />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </div>
-            </Router>
-            </ThemeProvider>
-        </Provider>
-    );
+  return (
+    <Wrapper>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <div className="app">
+              <Header />
+              <Routes>
+                <Route path="/" element={<EntryPage />} />
+                <Route path="/personal-info" element={<PersonalInfo />} />
+                <Route
+                  path="/salary-indications"
+                  element={<SalaryIndications />}
+                />
+                <Route path="/summary" element={<Summary />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </Router>
+        </ThemeProvider>
+      </Provider>
+    </Wrapper>
+  );
 };
 
 export default App;
---
+++ src/AppStyled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  padding-left: 2.5rem;
+  padding-right: 2.5rem;
+  max-width: 1710px;
+`;
