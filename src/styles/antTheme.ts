27b8738203522694222afe54f69871e8f68467cd
Liam <liam.going@code.berlin>
2024-09-06 15:45:18 +0200

    [update] antTheme override for fontSize and borderRadius

--- src/styles/antTheme.ts
+++ src/styles/antTheme.ts
@@ -4,5 +4,7 @@ export const antTheme = {
   token: {
     colorPrimary: theme.colors.black,
     colorPrimaryHover: theme.colors.darkGrey,
+    contentFontSizeLG: '24px',
+    borderRadiusLG: 30,
   },
 };
