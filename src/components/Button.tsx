cac54383b55cc9c89a65638c4479b9c5de849b58
Liam <liam.going@code.berlin>
2024-09-06 15:42:25 +0200

    [update] custom button props added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,20 +2,29 @@ import React from 'react';
 import { Button as AntButton } from 'antd';
 
 interface ButtonProps {
+  type: 'text' | 'link' | 'dashed' | 'default' | 'primary';
   text: string;
   onClick: () => void;
-  link?: string;
+  width?: number;
+  isDisabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, onClick, link }) => {
-  return link ? (
-    <a href={link} style={{ textDecoration: 'none' }}>
-      <AntButton type='primary' onClick={onClick}>
-        {text}
-      </AntButton>
-    </a>
-  ) : (
-    <AntButton type='primary' onClick={onClick}>
+const Button: React.FC<ButtonProps> = ({
+  type,
+  text,
+  onClick,
+  width,
+  isDisabled,
+}) => {
+  return (
+    <AntButton
+      type={isDisabled ? 'dashed' : type}
+      onClick={isDisabled ? undefined : onClick}
+      shape='round'
+      size='large'
+      style={{ width: width ? width : 120, height: 60 }}
+      disabled={isDisabled}
+    >
       {text}
     </AntButton>
   );
