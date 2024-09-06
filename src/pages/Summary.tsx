4cffe5ec04bc03e46f3aa5049fc88ecb9049c221
Liam <liam.going@code.berlin>
2024-09-06 15:46:49 +0200

    [update] Summary page rework and styling

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -1,38 +1,64 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import Button from '../components/Button';
 import ProgressIndicator from '../components/ProgressIndicator';
+import Card from '../components/Card';
+import { ButtonWrapper, ContentWrapper, TitleWrapper } from './shared/Styled';
+import Button from '../components/Button';
+import {
+  ItemWrapper,
+  TextWrapper,
+  LabelWrapper,
+  ValueWrapper,
+  DescriptionWrapper,
+} from './SummaryStyled';
 
 const Summary: React.FC = () => {
   const navigate = useNavigate();
-  const { fullName, email, phoneNumber, salary } = useSelector(
-    (state: any) => state.form
-  );
+  const {
+    fullName,
+    email,
+    phoneNumber,
+    salary,
+    streetAddress,
+    postalCode,
+    city,
+  } = useSelector((state: any) => state.form);
 
-  const handleEdit = () => {
-    navigate('/personal-info');
-  };
+  const handleEdit = () => navigate('/personal-info');
+  const handleSubmit = () => (window.location.href = 'https://www.buena.com');
+
+  const items = [
+    { label: 'Full Name:', value: fullName },
+    { label: 'Email:', value: email },
+    { label: 'Phone Number:', value: phoneNumber },
+    { label: 'Street Address:', value: streetAddress },
+    { label: 'Postal Code:', value: postalCode },
+    { label: 'City:', value: city },
+    { label: 'Salary Indications:', value: salary },
+  ];
 
   return (
-    <div>
-      <ProgressIndicator />
-
-      <h2>Summary</h2>
-      <p>
-        <strong>Full Name:</strong> {fullName}
-      </p>
-      <p>
-        <strong>Email:</strong> {email}
-      </p>
-      <p>
-        <strong>Phone Number:</strong> {phoneNumber}
-      </p>
-      <p>
-        <strong>Salary Indications:</strong> {salary}
-      </p>
-      <Button onClick={handleEdit}>Edit</Button>
-    </div>
+    <ContentWrapper>
+      <Card>
+        <ProgressIndicator />
+        <TitleWrapper>Summary</TitleWrapper>
+        <DescriptionWrapper>
+          <TextWrapper>
+            {items.map((item, index) => (
+              <ItemWrapper key={index}>
+                <LabelWrapper>{item.label}</LabelWrapper>
+                <ValueWrapper>{item.value || 'N/A'}</ValueWrapper>
+              </ItemWrapper>
+            ))}
+          </TextWrapper>
+        </DescriptionWrapper>
+        <ButtonWrapper>
+          <Button type='default' text='Edit' onClick={handleEdit} />
+          <Button type='primary' text='Submit' onClick={handleSubmit} />
+        </ButtonWrapper>
+      </Card>
+    </ContentWrapper>
   );
 };
 
