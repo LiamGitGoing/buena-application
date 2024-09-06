60e657ca4bdd8a19a971366a88f19eba1e9619e6
Liam <liam.going@code.berlin>
2024-09-06 15:48:30 +0200

    [update] salary page rework and styling

--- src/pages/SalaryIndications.tsx
+++ src/pages/SalaryIndications.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { Radio, Button as AntButton } from 'antd';
+import { Space } from 'antd';
 import { RadioChangeEvent } from 'antd/es/radio/interface';
 import { setSalary } from '../context/formSlice';
 import ProgressIndicator from '../components/ProgressIndicator';
+import Card from '../components/Card';
+import { ButtonWrapper, ContentWrapper, TitleWrapper } from './shared/Styled';
+import Button from '../components/Button';
+import {
+  RadioItem,
+  RadioStyled,
+  RadioWrapper,
+} from './SalaryIndicationsStyled';
 
 const SalaryIndications: React.FC = () => {
   const dispatch = useDispatch();
@@ -20,21 +28,39 @@ const SalaryIndications: React.FC = () => {
   };
 
   return (
-    <div>
-      <ProgressIndicator />
+    <ContentWrapper>
+      <Card>
+        <ProgressIndicator />
 
-      <h2>Salary Indications</h2>
-      <Radio.Group value={salary} onChange={handleChange}>
-        <Radio value="0-1'000">0-1'000</Radio>
-        <Radio value="1'000 - 2'000">1'000 - 2'000</Radio>
-        <Radio value="2'000 - 3'000">2'000 - 3'000</Radio>
-        <Radio value="3'000 - 4'000">3'000 - 4'000</Radio>
-        <Radio value="Mehr als 4'000">Mehr als 4'000</Radio>
-      </Radio.Group>
-      <AntButton type='primary' onClick={handleNext}>
-        Next
-      </AntButton>
-    </div>
+        <TitleWrapper>Your Salary Range</TitleWrapper>
+        <RadioWrapper>
+          <RadioStyled.Group value={salary} onChange={handleChange}>
+            <Space direction='vertical'>
+              <RadioItem>
+                <RadioStyled value="0-1'000">0-1'000</RadioStyled>
+              </RadioItem>
+              <RadioItem>
+                <RadioStyled value="1'000 - 2'000">1'000 - 2'000</RadioStyled>
+              </RadioItem>
+              <RadioItem>
+                <RadioStyled value="2'000 - 3'000">2'000 - 3'000</RadioStyled>
+              </RadioItem>
+              <RadioItem>
+                <RadioStyled value="3'000 - 4'000">3'000 - 4'000</RadioStyled>
+              </RadioItem>
+              <RadioItem>
+                <RadioStyled value="More than 4'000">
+                  More than 4'000
+                </RadioStyled>
+              </RadioItem>
+            </Space>
+          </RadioStyled.Group>
+        </RadioWrapper>
+        <ButtonWrapper>
+          <Button type='primary' text='Next' onClick={handleNext} />
+        </ButtonWrapper>
+      </Card>
+    </ContentWrapper>
   );
 };
 
