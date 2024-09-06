b41c0e932a474055f8268463a380e14097eabc20
Liam <liam.going@code.berlin>
2024-09-06 15:48:14 +0200

    [update] personal info page rework and styling

---
+++ src/pages/PersonalInfoStyled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const InputWrapper = styled.div`
+  display: flex;
+  padding: 10px 0;
+  width: 50%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FieldsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
